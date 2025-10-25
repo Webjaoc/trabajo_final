ca73fba97fce0492ccdfc244c8877143e7343c78
Webjaoc <Webjaoc>
2025-10-25 15:02:49 +0200

    Correccion de css

--- js/noticias.js
+++ js/noticias.js
@@ -1,38 +1,45 @@
 //Creamos una funcion asincrona con ajax para que cuando se cargue la pagina esta muestre las noticias del archivo Json.
 
-$(document).ready(function() {
-  $.ajax({
-    url: '../json/noticias.json',
-    method: 'GET',
-    dataType: 'json',
-    success: function(datos) {
-      mostrarNoticias(datos);
-    },
-    error: function(xhr, status, error) {
-      console.error('Error al cargar noticias:', error);
-      $('.listaNoticias').text('   No se pudieron cargar las noticias!!!');
-    }
-  });
-});
+const xhr = new XMLHttpRequest();
 
-function mostrarNoticias(datos) {
-  const cont = $('#listaNoticias');
-  cont.empty();
+xhr.open("GET", "../json/noticias.json", true);
 
-  if (datos.noticias && Array.isArray(datos.noticias)) {
-    datos.noticias.forEach(n => {
-      const article = `
-        <article>
+// Cuando la respuesta esté lista
+xhr.onload = function() {
+  if (xhr.status === 200) {
+    const datos = JSON.parse(xhr.responseText);
+    mostrarNoticias(datos.noticias);
+  } else {
+    console.error("Error al cargar las noticias:", xhr.status);
+  }
+};
+
+//errores 
+xhr.onerror = function() {
+  console.error("Error de conexión AJAX");
+};
+
+// Enviar la petición
+xhr.send();
+
+// Función para mostrar las noticias en el div
+function mostrarNoticias(noticias) {
+  const contenedor = document.getElementById("listaNoticias");
+  contenedor.innerHTML = "";
+
+  noticias.forEach(noticia => {
+    const item = document.createElement("div");
+    item.classList.add("noticia");
+    item.innerHTML = `
+      <article>
           <div class="article_noticias">
-            <h3>${n.titulo}</h3>
-            <time>${n.fecha}</time>
-            <p>${n.descripcion}</p>
+            <h3>${noticia.titulo}</h3>
+            <time>${noticia.fecha}</time>
+            <p>${noticia.descripcion}</p>
           </div>
         </article>`;
-      cont.append(article);
-    });
-  } else {
-    $('#listaNoticias').text('No hay noticias disponibles.');
-  }
+    contenedor.appendChild(item);
+  });
 }
+
 

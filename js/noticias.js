6667ca4e7b1352d1fa2e536e63456fa5640ab6f8
Webjaoc <Webjaoc>
2025-10-26 09:36:49 +0100

    cargando estilos 2

--- js/noticias.js
+++ js/noticias.js
@@ -1,25 +1,27 @@
 //Aqui creamos lafuncion ajax para cargar los datos de manera asincrona
 
 function cargar() {
+  let listaNoticias = document.getElementById('listaNoticias');
+  let errorNoticias = document.querySelector('.listaNoticias');
   $.ajax({
     url: './json/noticias.json',
     type: 'GET',
-    dataType: 'json', // importante
+    dataType: 'json',
     success: function (objeto_json) {
       let cadena = "";
     
       for (let i = 0; i < objeto_json.noticias.length; i++) {
-        cadena = cadena +"<div class='article_noticias'>"
+        cadena = cadena +"<div class='article_noticias'>";
         cadena = cadena + "<h3>" + objeto_json.noticias[i].titulo + "</h3>";
         cadena = cadena + "<time>" + objeto_json.noticias[i].fecha +"</time><br>";
         cadena = cadena + "<p>" + objeto_json.noticias[i].descripcion +"</p><hr>";
-        cadena = cadena + "</div>"
+        cadena = cadena + "</div>";
       }
 
-      $("#listaNoticias").html(cadena);
+      $(listaNoticias).html(cadena);
     },
     error: function (xhr, status) {
-      alert('Disculpe, existió un problema');
+      errorNoticias.innerHTML='Disculpe, existió un problema';
     }
   });
 }
